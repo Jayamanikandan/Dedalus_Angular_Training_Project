ffaee42b2785730670dd2749bf1dd751761ce9a3
jayamanikandan <jayamanikandan@yhoo.com>
2023-07-04 10:13:11 +0530

    checking-in final project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductlistComponent } from './views/productlist/productlist.component';
+import { LogonComponent } from './logon/logon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"logon", component: LogonComponent},
+  {path:"user",
+  loadChildren:() => import('./user/user.module').then(m => m.UserModule)},
+  {path:"admin",
+  loadChildren:() => import('./user/user.module').then(m => m.UserModule)}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogonComponent } from './logon/logon.component';
+import { UserModule } from './user/user.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogonComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    FormsModule,
+    BrowserAnimationsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
